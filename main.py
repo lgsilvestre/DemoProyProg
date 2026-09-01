da210c1ddd3987cfde40ba507ae459c7b5c412cd
BaltazarEspinoza <balta.espinoza@gmail.com>
2026-09-01 17:12:13 -0400

    Funcion Calculadora genial

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-a=2
+Luis = 1
+Allan = '+'
+Balta = 3
+
+def calcular(n1, n2, op):
+  if op == '+':
+    return n1 + n2
+  elif op == '-':
+    return n1 - n2
+  elif op == '*':
+    return n1 * n2
+  elif op == '/':
+    if n2 == 0:
+      return 'Error: División por cero'
+    return n1 / n2
+  else:
+    return 'Operador no válido'
 
 
-# Este es un comentario propio de la rama Balta :p
-Variable_de_rama_balta = "Hola mundo"
+# Ejemplo de uso:
+resultado = calcular(Luis,Balta,Allan)
+print(resultado)  # Muestra 15
+
+
+calcular(Luis,Balta,Allan)
